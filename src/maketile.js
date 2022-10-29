1a7bb7bab79223b222f64398e05b356d953a6d0c
Renee Sonntag <waverly@vivaldi.net>
2022-10-29 17:27:23 -0500

    Color picking works now

--- src/maketile.js
+++ src/maketile.js
@@ -1,8 +1,9 @@
-var opt = 1
+/* Author: R. W. Sonntag
+ */
+
+var can = document.getElementById('color-tile')
 
 function maketile() {
-   let one = performance.now()
-
    let l_luma = {R: luma[lumaselect.value].R, G: luma[lumaselect.value].G, B: luma[lumaselect.value].B}
    tred.innerText = l_luma.R
    tgreen.innerText = l_luma.G
@@ -12,8 +13,7 @@ function maketile() {
 
    let shape = shaper.value
 
-   var can = document.getElementById('color-tile')
-   var ctx = can.getContext('2d')
+   var ctx = can.getContext('2d', {colorSpace: "srgb"})
    var tri
 
    var newimage = ctx.createImageData(can.width, can.height)
@@ -31,13 +31,44 @@ function maketile() {
             roundedY = Math.floor(s / 2.0 * Math.sin(2.0 * 3.141592653589 * (h / 255.0))) + 128
          }
          let index = 4 * (can.width * roundedY + roundedX)
-         newctx[index + 0] = delinearize(tri[0])*255
-         newctx[index + 1] = delinearize(tri[1])*255
-         newctx[index + 2] = delinearize(tri[2])*255
+         newctx[index + 0] = Math.round(delinearize(tri[0])*255)
+         newctx[index + 1] = Math.round(delinearize(tri[1])*255)
+         newctx[index + 2] = Math.round(delinearize(tri[2])*255)
          newctx[index + 3] = 255
       }
    }
 
    ctx.putImageData(newimage, 0, 0)
-   console.log(performance.now()-one)
 }
+
+function getPickedColor(canv, eve) {
+   let rect = canv.getBoundingClientRect()
+   let x = eve.clientX - rect.left
+   let y = eve.clientY - rect.top
+
+   let l_luma = {R: luma[lumaselect.value].R, G: luma[lumaselect.value].G, B: luma[lumaselect.value].B}
+   let res
+   if (shaper.value == 'circle') {
+      x = (x - 128) * 2
+      y = (y - 128) * 2
+      console.log(x, y)
+      let arctangent = Math.atan(y / x)
+
+      /* Convert arctan regions into position accurate positive numbers */
+      if (x < 0) {
+         arctangent += 3.141592653589
+      } else if (y < 0) {
+         arctangent += 2 * 3.141592653589
+      }
+
+      res = HSYToRGB(((arctangent / 2.0) / 3.141592653589), Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / 255.0, lumaval.value / 255.0, l_luma.R, l_luma.G, l_luma.B)
+   } else if (shaper.value == 'square') {
+      res = HSYToRGB(x/255.0, y/255.0, lumaval.value / 255.0, l_luma.R, l_luma.G, l_luma.B)
+   }
+   console.log(res)
+   // Don't forget to delinearize!
+   document.body.style.background = 'rgb(' + Math.round(delinearize(res[0])*255) + ',' + Math.round(delinearize(res[1])*255) + ',' + Math.round(delinearize(res[2])*255) + ')'
+   coldump.innerText = document.body.style.background
+}
+
+can.addEventListener('mousedown', function(e) { getPickedColor(can, e) })
